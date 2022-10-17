5b776e45d3150e4345affee9593706cd33964120
Daniel Russom <daniel.russom00@gmail.com>
2022-10-17 17:03:25 +0100

    Resolve book stream duplication

--- src/com/codurance/solid/ItBookDiscount.java
+++ src/com/codurance/solid/ItBookDiscount.java
@@ -2,27 +2,24 @@ package com.codurance.solid;
 
 import static com.codurance.solid.BookType.IT;
 
+import java.util.stream.Stream;
+
 public class ItBookDiscount implements IBookDiscount {
 
 	@Override
 	public double getDiscountedPrice(BookCollection books) {
+		double number_of_it_books = getItBooks(books).toList().size();
+		double total_price_for_it_books = getItBooks(books).mapToDouble(Book::price).sum();
 		double it_books_discount = 0;
-		double number_of_it_books = 0;
-		double total_price_for_it_books = 0;
 		
-		number_of_it_books = books.getBooks().stream().filter(book -> book.type().equals(IT)).count();
-		
-		for (Book book : books.getBooks()) {
-			if (IT.equals(book.type())) {
-				total_price_for_it_books += book.price();
-			}
-		}
-		
-		if (number_of_it_books > 2) {
-			it_books_discount = 0.7; // 30% discount when buying more than 2 IT books
-		} else if (number_of_it_books > 0) {
+		if (number_of_it_books > 0) {
 			it_books_discount = 0.9; // 10% discount when buying up to 2 IT books
 		}
+
+		if (number_of_it_books > 2) {
+			it_books_discount = 0.7; // 30% discount when buying more than 2 IT books
+		} 
+		
 		if (it_books_discount > 0) {
 			total_price_for_it_books *= it_books_discount;
 		}
@@ -30,4 +27,8 @@ public class ItBookDiscount implements IBookDiscount {
 		return total_price_for_it_books;
 	}
 
+	private Stream<Book> getItBooks(BookCollection books) {
+		var itBooks = books.getBooks().stream().filter(book -> book.type().equals(IT));
+		return itBooks;
+	}
 }
--- src/com/codurance/solid/TravelBookDiscount.java
+++ src/com/codurance/solid/TravelBookDiscount.java
@@ -2,22 +2,16 @@ package com.codurance.solid;
 
 import static com.codurance.solid.BookType.TRAVEL;
 
+import java.util.stream.Stream;
+
 public class TravelBookDiscount implements IBookDiscount {
 
 	@Override
 	public double getDiscountedPrice(BookCollection books) {
+		double number_of_travel_books = getTravelBooks(books).count();
+		double total_price_for_travel_books = getTravelBooks(books).mapToDouble(Book::price).sum();
 		double travel_books_discount = 0;
-		double number_of_travel_books = 0;
-		double total_price_for_travel_books = 0;
-
-		number_of_travel_books = books.getBooks().stream().filter(book -> book.type().equals(TRAVEL)).count();
-			
-		for (Book book : books.getBooks()) {
-			if (TRAVEL.equals(book.type())) {
-				total_price_for_travel_books += book.price();
-			}
-		}
-
+		
 		if (number_of_travel_books > 3) {
 			travel_books_discount = 0.6; // 40% discount when buying more than 3 travel books
 		}
@@ -29,4 +23,8 @@ public class TravelBookDiscount implements IBookDiscount {
 		return total_price_for_travel_books;
 	}
 
+	private Stream<Book> getTravelBooks(BookCollection books) {
+		var itBooks = books.getBooks().stream().filter(book -> book.type().equals(TRAVEL));
+		return itBooks;
+	}
 }
